c5de929ba0694b8422c9acb8eaa1e93309d69b00
Soyer1310 <soyer131313@gmail.com>
2022-05-20 21:17:59 +0300

    add capitalize.js

---
+++ src/capitalize.js
@@ -0,0 +1,7 @@
+const capitalize = (string) => {
+    if (string === '') {
+        return '';
+    }
+    const [firstChar, ...rest] = string;
+    return `${firstChar.toUpperCase()}${rest.join()}`;
+}
